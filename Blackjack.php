c8a80dbb9b7c02d6720495562f24951bf839b50a
Peter <dhondt.peter@gmail.com>
2021-05-23 15:44:11 +0200

    finished the Player class

--- Blackjack.php
+++ Blackjack.php
@@ -9,7 +9,7 @@ class Blackjack
 
     public function __construct()
     {
-        $this->player = new Player();
+        $this->player = new Player($this->deck);
         $this->dealer = new Player();
         $this->deck = new Deck();
         $this->deck->shuffle();
--- Player.php
+++ Player.php
@@ -1,33 +1,44 @@
 <?php
 declare(strict_types=1);
 
+require 'Deck.php';
+
 class Player
 {
+    private const MAX_SCORE = 21;
     private array $cards;
     private bool $lost = false;
 
-    public function __construct()
+    public function __construct(Deck $deck)
     {
-        
+        $this->cards[] = $deck->drawCard();
+        $this->cards[] = $deck->drawCard();
     }
 
-    public function hit()
+    public function hit(Deck $deck)
     {
-
+        $this->cards[] = $deck->drawCard();
+        if ($this->getScore() > self::MAX_SCORE) {
+            $this->lost = true;
+        }
     }
 
     public function surrender()
     {
-
+        $this->lost = true;
     }
 
     public function getScore()
     {
+        foreach ($this->cards as $card) {
+            $score += $card->getValue();
+        }
+        return $score;
 
     }
 
     public function hasLost()
     {
-
+        return $this->lost;
     }
 }
