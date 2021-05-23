30e1e57c344e6dfa1270dc3973dc31f83cebd7c1
Peter <dhondt.peter@gmail.com>
2021-05-23 12:38:24 +0200

    created base classes

---
+++ Blackjack.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+class Blackjack
+{
+    private Player $player;
+    private Player $dealer;
+    private Deck $deck;
+
+    public function __construct()
+    {
+        $this->player = new Player();
+        $this->dealer = new Player();
+        $this->deck = new Deck();
+        $this->deck->shuffle();
+    }
+
+    public function getPlayer(): Player
+    {
+
+    }
+
+    public function getDealer(): Player
+    {
+
+    }
+
+    public function getDeck(): Deck
+    {
+
+    }
+
+}
